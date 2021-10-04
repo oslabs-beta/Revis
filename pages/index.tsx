957f10b9599118497d54e61698ea7616836d2346
mkalaizic <kalaizicmercedes@gmail.com>
2021-10-04 16:12:35 -0400

    hi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,23 @@
-<<<<<<< HEAD
 import RightSideLogin from '../components/UserLogin';
 import LogoHomeScreen from '../components/LeftSideHomeScreen';
 import Link from 'next/link';
-import { useContext, } from 'react';
-=======
+
 import UserLogin from '../components/UserLogin';
 import ForgotPassword from '../components/ForgotPassword';
 import SignUp from '../components/SignUp';
 import { useContext, useState } from 'react';
->>>>>>> f8086ce8b593b1073c55c5cffcda6ea2f33e7ac7
 import { GlobalContext } from '../context/Provider';
 import styles from '../styles/RightSideLogin.module.scss';
 
 
 function HomePage() {
-<<<<<<< HEAD
 
-  const logIn = (userInfo) => {
-=======
   const [pages, setPages] = useState<string>('userLogin');
-  const testingState = useContext(GlobalContext);
   const login = (userInfo) => {
->>>>>>> f8086ce8b593b1073c55c5cffcda6ea2f33e7ac7
     //fetch here
     console.log(userInfo);
   };
 
-<<<<<<< HEAD
-  
-  return (
-    <div className= "homePageWrapper">
-      <div>
-      <LogoHomeScreen />
-      </div>
-      <div>
-        <RightSideLogin logIn={logIn} />
-     
-     
-        <Link href='/ForgotPassword'>
-          <button>Forgot Password?</button>
-        </Link>
-     
-     
-        <Link href='/SignUp'>
-          <button>SignUp</button>
-        </Link>
-      </div>
-      </div>
-  )
-};
-=======
   const previousPage = () => {
     setPages('userLogin');
   };
@@ -89,6 +57,6 @@ function HomePage() {
       );
   }
 }
->>>>>>> f8086ce8b593b1073c55c5cffcda6ea2f33e7ac7
+
 
 export default HomePage;
