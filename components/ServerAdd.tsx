87c5d70becc9e0d39cd963f08fbc5e3cb2fbcc13
Liam Fontes <liamfontes1244@gmail.com>
2021-10-05 15:36:01 -0400

    Updated ServerAdd

--- components/ServerAdd.tsx
+++ components/ServerAdd.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../styles/ServerAdd.module.scss';
-import isIp from 'is-ip';
 
 function ServerAdd(props) {
   const { addServer } = props;
