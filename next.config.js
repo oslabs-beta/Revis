33484bd13da38570dce8484f5983ada69a4d6038
Chao Yu <ChaoY91@yahoo.com>
2021-10-05 12:12:39 -0400

    Redis implementation

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   env: {
     PG_URI:
       'postgres://jlrxiqce:5CjZQ27IV1uExmsP-t_O-ZPPFA0NDmsU@fanny.db.elephantsql.com/jlrxiqce',
+     
+    REDIS_URL: 'redis-18891.c9.us-east-1-4.ec2.cloud.redislabs.com:18891',
   },
 };
