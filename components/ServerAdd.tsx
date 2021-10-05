875effd7703684f57c843c4ea09cfc811c675ea2
LiamFontes <liamfontes1244@gmail.com>
2021-10-05 08:25:18 -0400

    Continued progress on dashboard collapsable sidebar

--- components/ServerAdd.tsx
+++ components/ServerAdd.tsx
@@ -1,22 +1,66 @@
-import React, { useState } from 'react';
-import styles from '../styles/ServerAdd.module.scss';
+import React, { useEffect, useState } from "react";
+import styles from "../styles/ServerAdd.module.scss";
 
 function ServerAdd(props) {
   const { addServer } = props;
+
+  const IP_REG_EX = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(.|$)){4}";
+  const PORT_REG_EX = "[0-9]{4}";
+  const validityCheck = (e) => {
+    const currentElem: HTMLInputElement = e.target;
+    if (!currentElem.validity.valid) {
+      if (currentElem.id === "name") {
+        currentElem.nextSibling.innerHTML =
+          "Please input at least four characters";
+      } else if (currentElem.id === "PORT") {
+        currentElem.nextSibling.innerHTML =
+          "Please input a proper port number (eg. 8080)";
+      } else {
+        currentElem.nextSibling.innerHTML =
+          "Please input a valid IP address (ie. 192.91.44.23)";
+      }
+    } else currentElem.nextSibling.innerHTML = "";
+  };
   return (
     <div className={styles.serverAddWrapper}>
       <h1> Add Server </h1>
-      <form onSubmit={addServer}>
+      <form onSubmit={addServer} noValidate>
         <div className={styles.inputWrapper}>
           <label>Name:</label>
-          <input type="text" id="name"></input>
+          <input
+            type="text"
+            id="name"
+            autoComplete="off"
+            required
+            onChange={validityCheck}
+            minLength={4}
+          ></input>
+          <span></span>
+
           <br />
 
           <label>IP:</label>
-          <input type="text" id="IP"></input>
+          <input
+            type="text"
+            id="IP"
+            autoComplete="off"
+            required
+            onChange={validityCheck}
+            // minLength={7}
+            pattern={IP_REG_EX}
+          ></input>
+          <span></span>
           <br />
           <label>Port:</label>
-          <input type="text" id="PORT"></input>
+          <input
+            type="text"
+            id="PORT"
+            autoComplete="off"
+            required
+            onChange={validityCheck}
+            pattern={PORT_REG_EX}
+          ></input>
+          <span></span>
         </div>
         <input type="submit" value="Add Server" />
       </form>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import ServerAdd from './ServerAdd';
-import ServerList from './ServerList';
-import styles from '../styles/Sidebar.module.scss';
+import React, { useState } from "react";
+import Link from "next/link";
+import ServerAdd from "./ServerAdd";
+import ServerList from "./ServerList";
+import styles from "../styles/Sidebar.module.scss";
 
 function Sidebar(props) {
   const [sideBarHidden, showOrHideSideBar] = useState(false);
@@ -10,10 +10,12 @@ function Sidebar(props) {
 
   const addServer = (e) => {
     e.preventDefault();
-    const name = document.querySelector('#name').value;
-    const IP = document.querySelector('#IP').value;
-    const PORT = document.querySelector('#PORT').value;
-    updateList(serverList.concat({ name, IP, PORT }));
+    const name = document.querySelector("#name").value;
+    const IP = document.querySelector("#IP").value;
+    const PORT = document.querySelector("#PORT").value;
+
+    if (name.length <= 4 && PORT.length >= 4 && IP.length >= 7)
+      updateList(serverList.concat({ name, IP, PORT }));
   };
 
   return (
