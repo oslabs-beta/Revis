35d74866501528193a63f0067a7a7b607ab489e1
mkalaizic <kalaizicmercedes@gmail.com>
2021-10-07 19:14:06 -0400

    merging to chaos

--- pages/api/redis.js
+++ pages/api/redis.js
@@ -1,57 +1,57 @@
-// const Redis = require('ioredis');
-// const useStore = require('../../context/Provider');
+const Redis = require('ioredis');
+const useStore = require('../../context/Provider');
 
-//  const metrics = async (req, res) => {
-//   const metricsToEvaluate = [
+ const metrics = async (req, res) => {
+  const metricsToEvaluate = [
 
-//     'total_net_output_bytes',
-//     'used_memory',
-//     'connected_clients',
-//     'evicted_keys',
-//     'keyspace_hits',
-//     'keyspace_misses',
-//     'total_net_input_bytes',
-//     'uptime_in_seconds', // how long you've been at the server for
+    'total_net_output_bytes',
+    'used_memory',
+    'connected_clients',
+    'evicted_keys',
+    'keyspace_hits',
+    'keyspace_misses',
+    'total_net_input_bytes',
+    'uptime_in_seconds', // how long you've been at the server for
 
-//   ];
-//   const redis = new Redis({
-//     host: process.env.REDIS_URL,
-//     port: process.env.REDIS_PORT,
-//     password: process.env.REDIS_PW,
-//   });
+  ];
+  const redis = new Redis({
+    host: process.env.REDIS_URL,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PW,
+  });
 
-//   async function creatingMetricsObject() {
-//     let data = await redis.info();
+  async function creatingMetricsObject() {
+    let data = await redis.info();
 
-//     data = data.split("\r\n");
+    data = data.split("\r\n");
 
-//     const objWithMetrics = {};
-//     data.forEach((el) => {
-//       const keysAndValues = el.split(":");
+    const objWithMetrics = {};
+    data.forEach((el) => {
+      const keysAndValues = el.split(":");
 
-//       if (metricsToEvaluate.includes(keysAndValues[0]))
-//         objWithMetrics[keysAndValues[0]] = keysAndValues[1];
-//     });
+      if (metricsToEvaluate.includes(keysAndValues[0]))
+        objWithMetrics[keysAndValues[0]] = keysAndValues[1];
+    });
 
-//     return objWithMetrics;
-//   }
+    return objWithMetrics;
+  }
 
-//   const { method } = req;
+  const { method } = req;
 
-//   switch (method) {
-//     case 'GET':
-//       try {
-//         // console.log('this is working')
-//         const metricsUpdated = await creatingMetricsObject();
-//         console.log(metricsUpdated);
-//         redis.quit(() => {
-//           console.log('exited redis server');
-//         });
-//         res.status(200).send(metricsUpdated);
-//       } catch {
-//         console.log('error in getting metrics');
-//       }
-//   }
-// };
+  switch (method) {
+    case 'GET':
+      try {
+        // console.log('this is working')
+        const metricsUpdated = await creatingMetricsObject();
+        console.log(metricsUpdated);
+        redis.quit(() => {
+          console.log('exited redis server');
+        });
+        res.status(200).send(metricsUpdated);
+      } catch {
+        console.log('error in getting metrics');
+      }
+  }
+};
 
-// export default metrics;
+export default metrics;
