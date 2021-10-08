e57906e104a22bf71847b71ebf9789e24e38c7d7
mkalaizic <kalaizicmercedes@gmail.com>
2021-10-08 18:40:21 -0400

    serversToGlobalState

---
+++ context/initialStates/initialStateServers.tsx
@@ -0,0 +1,12 @@
+const Servers = {
+  serverList: [
+    {
+      name: "Mercedes",
+      IP: "redis-16424.c289.us-west-1-2.ec2.cloud.redislabs.com",
+      PORT: "16424",
+      username: "prueba",
+    },
+  ],
+};
+
+export default Servers;
--- pages/redisinfo.tsx
+++ pages/redisinfo.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BackButton from "../components/BackButton";
+import ServerListFromState from "../components/ServerListFromState";
 
 function redisInfo() {
   return (
     <div>
       <BackButton />
+      <ServerListFromState />
     </div>
   );
 }
