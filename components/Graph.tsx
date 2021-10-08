cc89ae6a3bd3005263537ccfe7ec2ad474b47109
mkalaizic <kalaizicmercedes@gmail.com>
2021-10-08 18:50:53 -0400

    graphComponent

---
+++ components/Graph.tsx
@@ -0,0 +1,6 @@
+function Graph {
+    return(
+        <img src="https://i.pinimg.com/originals/2e/e6/99/2ee6998e34c3e2eff7b894c66cfc5267.jpg"></img>
+    )
+}
+export defaunt Graph;
--- pages/redisinfo.tsx
+++ pages/redisinfo.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Graph from "../components/Graph";
 import BackButton from "../components/BackButton";
 
 function redisInfo() {
   return (
     <div>
       <BackButton />
+      <Graph />
     </div>
   );
 }
