c560b3270c1dfdae2140129f40dd888865dff4bd
mkalaizic <kalaizicmercedes@gmail.com>
2021-10-08 18:41:35 -0400

    serversToGlobalState

--- pages/redisinfo.tsx
+++ pages/redisinfo.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import BackButton from "../components/BackButton";
-import ServerListFromState from "../components/ServerListFromState";
 
 function redisInfo() {
   return (
     <div>
       <BackButton />
-      <ServerListFromState />
     </div>
   );
 }
