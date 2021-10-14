749e04b32451919e2549793b8a54f9ea1199c05a
Liam Fontes <liamfontes1244@gmail.com>
2021-10-14 15:35:15 -0400

    Updated metric text in summary table

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -5,19 +5,28 @@ import { useStore } from '../context/Provider';
 import styles from '../styles/Summary.module.scss';
 
 interface MetricsProps {
-  keys: string[];
-  values: string[];
+  keys: string;
+  values: string;
 }
 
 export default function Metrics(props) {
   const { keys, values }: MetricsProps = props;
   const { metricToGraph } = useStore();
 
+  const cleanKeys = (string) => {
+    const splitKeys = string.split('_');
+    const updatedKeys = splitKeys.map((str) => {
+      const firstLetter = str[0].toUpperCase();
+      return firstLetter + str.slice(1);
+    });
+    return updatedKeys;
+  };
+
   return (
     <div className={styles.metrics}>
-      <h5>{keys}</h5>
+      <h5>{cleanKeys(keys).join(' ')}</h5>
       <button
-        type='button'
+        type="button"
         onClick={() => {
           metricToGraph.selectedMetricDispatch({
             type: 'updateSelectedMetric',
