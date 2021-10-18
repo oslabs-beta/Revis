658a03d4400ee053de5d6de7dc2a38a9d7e0bf5b
mkalaizic <kalaizicmercedes@gmail.com>
2021-10-18 16:05:37 -0400

    change of renders1

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,14 +1,18 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
-import router from 'next/router';
-import { useStore } from '../../context/Provider';
-import Sidebar from './Sidebar';
-import Summary from './Summary';
-import styles from '../../styles/Dashboard.module.scss';
-import SignOutButton from '../Globals/SignOutButton';
-import { Context } from '../../context/interfaces';
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import router from "next/router";
+import { useStore } from "../../context/Provider";
+import Sidebar from "./Sidebar";
+import Summary from "./Summary";
+import styles from "../../styles/Dashboard.module.scss";
+import SignOutButton from "../Globals/SignOutButton";
+import NavBarDashboard from "./NavBarDashboard";
+import { Context } from "../../context/interfaces";
+import Graph from "../Graphs/Singular/Graph";
+import MultipleGraphContainer from "../Graphs/Multiple/MultipleGraphContainer";
 
 export default function Dashboard() {
   const { user }: Context = useStore();
+  const [currentRender, setCurrentRender] = useState("dashboard");
   const [noUsername, changeUsernameBool]: [
     boolean,
     Dispatch<SetStateAction<boolean>>
@@ -16,30 +20,92 @@ export default function Dashboard() {
   const { userDispatch } = user;
 
   useEffect(() => {
-    fetch('/api/validateUser')
+    fetch("/api/validateUser")
       .then((response: Response) => response.json())
       .then((data) => {
         const { username, ssid }: { username: string; ssid: string } = data;
-        if (!username || !ssid) return router.replace('/');
-        userDispatch({ type: 'updateUsername', message: username });
+        if (!username || !ssid) return router.replace("/");
+        userDispatch({ type: "updateUsername", message: username });
         changeUsernameBool(false);
       })
       .catch((err) => console.log(err));
   }, []);
-  return (
-    <div className={styles.dashboardWrapper}>
-      {!noUsername && (
-        <>
-          <div className={styles.sidebarWrapper}>
-            <SignOutButton />
-            <Sidebar />
-          </div>
 
-          <div className={styles.summaryWrapper}>
-            <Summary />
-          </div>
-        </>
-      )}
-    </div>
-  );
+  const viewGraph = () => {
+    setCurrentRender("graph");
+  };
+  const viewMultipleGraphs = () => {
+    setCurrentRender("multipleGraphs");
+  };
+  const viewDashboard = () => {
+    setCurrentRender("dashboard");
+  };
+
+  switch (currentRender) {
+    case "graph":
+      return (
+        <div className={styles.dashboardWrapper}>
+          {!noUsername && (
+            <>
+              <div className={styles.sidebarWrapper}>
+                <NavBarDashboard
+                  viewGraph={viewGraph}
+                  viewMultipleGraphs={viewMultipleGraphs}
+                  viewDashboard={viewDashboard}
+                />
+                <SignOutButton />
+              </div>
+              <Sidebar />
+              <div className={styles.summaryWrapper}>
+                <Graph />
+              </div>
+            </>
+          )}
+        </div>
+      );
+    case "multipleGraphs":
+      return (
+        <div className={styles.dashboardWrapper}>
+          {!noUsername && (
+            <>
+              <div className={styles.sidebarWrapper}>
+                <NavBarDashboard
+                  viewGraph={viewGraph}
+                  viewMultipleGraphs={viewMultipleGraphs}
+                  viewDashboard={viewDashboard}
+                />
+                <SignOutButton />
+                <Sidebar />
+              </div>
+
+              <div className={styles.summaryWrapper}>
+                <MultipleGraphContainer />
+              </div>
+            </>
+          )}
+        </div>
+      );
+    default:
+      return (
+        <div className={styles.dashboardWrapper}>
+          {!noUsername && (
+            <>
+              <div className={styles.sidebarWrapper}>
+                <NavBarDashboard
+                  viewGraph={viewGraph}
+                  viewMultipleGraphs={viewMultipleGraphs}
+                  viewDashboard={viewDashboard}
+                />
+                <SignOutButton />
+                <Sidebar />
+              </div>
+
+              <div className={styles.summaryWrapper}>
+                <Summary />
+              </div>
+            </>
+          )}
+        </div>
+      );
+  }
 }
