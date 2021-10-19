fb475586b695212135fe25e34502fe10f7588980
Liam Fontes <liamfontes1244@gmail.com>
2021-10-18 22:00:31 -0400

    Continued progress on storing redis data in Redis. Started work on metric history feature

---
+++ src/context/reducers/metricHistory.tsx
@@ -0,0 +1,39 @@
+import { MetricHistory, ActionMetricHistory } from '../interfaces';
+
+const deleteServerFromDataBase = (name: string) => {
+  fetch('/api/servers', {
+    method: 'DELETE',
+    body: JSON.stringify({ name }),
+    'Content-Type': 'application/json',
+  });
+};
+
+const postServerToDataBase = (
+  name: string,
+  endpoint: string,
+  port: string,
+  username: string,
+  password: string
+) => {
+  fetch('/api/servers', {
+    method: 'POST',
+    body: JSON.stringify({ name, endpoint, port, username, password }),
+    'Content-Type': 'application/json',
+  });
+};
+
+const metricHistory = (state: MetricHistory, action: ActionMetricHistory) => {
+  const server: MetricHistory = action.message;
+  switch (action.type) {
+    case 'addServer': {
+    }
+    case 'deleteServer': {
+    }
+    case 'populateList': {
+    }
+    default:
+      return state;
+  }
+};
+
+export default metricHistory;
