c29ad784e9c62d1b773a60a09ac4e6dcbd7f3dcc
Chao Yu <ChaoY91@yahoo.com>
2021-10-21 12:57:34 -0400

    metrics unit conversion bytes to MB and seconds to hours

--- src/context/reducers/metrics.tsx
+++ src/context/reducers/metrics.tsx
@@ -9,7 +9,22 @@ const metrics = (state: Metrics[], action: ActionMetrics) => {
 
   switch (action.type) {
     case 'updateMetrics':
-      console.log(metricsList);
+      action.message.uptime_in_seconds = Number(
+        action.message.uptime_in_seconds / 3600
+      ).toFixed(0);
+
+      action.message.used_memory = Number(
+        action.message.used_memory * 1e-6
+      ).toFixed(2);
+
+      action.message.total_net_output_bytes = Number(
+        action.message.total_net_output_bytes * 1e-6
+      ).toFixed(2);
+
+      action.message.total_net_input_bytes = Number(
+        action.message.total_net_input_bytes * 1e-6
+      ).toFixed(2);
+
       metricsList.push(action.message);
       return metricsList;
     default:
