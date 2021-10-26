ce823b491491121abaac4f11694e9d3d16b98619
Liam Fontes <liamfontes1244@gmail.com>
2021-10-26 14:59:55 -0400

    No longer required to have metrics initial state. If user has existing records from the day, then that data will be populated into state. Refactored code in several components to handle edge cases with missing data

--- src/components/Dashboard/CustomMetricDropdown.tsx
+++ src/components/Dashboard/CustomMetricDropdown.tsx
@@ -1,38 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { useStore } from '../../context/Provider';
 import styles from '../../styles/CustomMetricDropdown.module.scss';
 
 function CustomMetricDropdown(props) {
   const { metricName } = props;
   const [update, setUpdate] = useState(false);
+  const [metricOptions, updateOptions] = useState([]);
   const { metricsStore, customMetrics } = useStore();
   const { customMetricState, customMetricDispatch } = customMetrics;
   const { metricState } = metricsStore;
-  const metricOptions = Object.keys(metricState[0]);
+
   const updateMetrics = (metric: string) => {
     setUpdate(!update);
     if (metric in customMetricState) return;
     customMetricDispatch({
       type: 'changeMetric',
-      message: { 
+      message: {
         deletedMetric: metricName,
         updatedMetric: metric,
-      }
+      },
     });
-  }
+  };
+  useEffect(() => {
+    if (!metricState) return;
+    updateOptions(Object.keys(metricState[0]));
+  }, [metricState]);
+
   return (
     <div className={styles.dropdown}>
-      <div className={styles.dropdownBtn} onClick={() => setUpdate(!update)}>{metricName}
+      <div className={styles.dropdownBtn} onClick={() => setUpdate(!update)}>
+        {metricName}
         <FontAwesomeIcon icon={faCaretDown} />
       </div>
       {update && (
         <div className={styles.dropdownContent}>
           {metricOptions.map((metric) => (
             <div
-              onClick={() => { updateMetrics(metric) }}
+              onClick={() => {
+                updateMetrics(metric);
+              }}
               className={styles.dropdownItem}
             >
               {metric}
@@ -46,6 +55,6 @@ function CustomMetricDropdown(props) {
 
 CustomMetricDropdown.propTypes = {
   metricName: PropTypes.string.isRequired,
-}
+};
 
 export default CustomMetricDropdown;
