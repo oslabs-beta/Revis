4d7bbb5ced8f2db0ee1c989a9c7cdd4b626faf05
Chao Yu <ChaoY91@yahoo.com>
2021-10-26 16:51:38 -0400

    dockerization

--- src/components/Graphs/Multiple/LeftMenuOfGraph.tsx
+++ src/components/Graphs/Multiple/LeftMenuOfGraph.tsx
@@ -7,13 +7,14 @@ import MetricsForGraph from './MetricsForGraph';
 function LeftMenuOfGraph() {
   const { metricsStore, multipleGraphSelections }: Context = useStore();
   const metricsForCheckBoxes: ReactElement[] = [];
-
-  Object.entries(metricsStore.metricState[0]).forEach((el) => {
-    if (el[0] !== 'time')
-      metricsForCheckBoxes.push(<MetricsForGraph metricName={el[0]} />);
-  });
+  if (Object.keys(metricsStore.metricState).length !== 0) {
+    Object.entries(metricsStore.metricState[0]).forEach((el) => {
+      if (el[0] !== 'time')
+        metricsForCheckBoxes.push(<MetricsForGraph metricName={el[0]} />);
+    });
+  }
   return (
-    <div id="leftMenuGraphs" className={styles.LeftMenu}>
+    <div id='leftMenuGraphs' className={styles.LeftMenu}>
       {Object.keys(multipleGraphSelections.multipleGraphState).length > 3 ? (
         <div className={styles.MaxGraphsMessage}>
           Only 4 graphs can be simultaneously displayed
