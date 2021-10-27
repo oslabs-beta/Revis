b6fc82faaf9be6b88ee1a7adeca6274bf71e7639
mkalaizic <kalaizicmercedes@gmail.com>
2021-10-27 17:53:39 -0400

    signout

--- src/components/Globals/Welcome.tsx
+++ src/components/Globals/Welcome.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Context } from '../../context/interfaces';
 import { useStore } from '../../context/Provider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
 import styles from '../../styles/Welcome.module.scss';
 
 function Welcome() {
   const { user }: Context = useStore();
-
+  const signOut = () => {
+    fetch('/api/signOut').then(() => router.replace('/'));
+  };
+  function showingDropdown() {
+    document
+      .querySelector(`.${styles.dropdowncontent}`)
+      .classList.toggle(`${styles.show}`);
+  }
   return (
     <div className={styles.Welcome}>
       <h4>Signed in as </h4>
-      <h5>{user.userState.username}</h5>
+      <button type='button' id={styles.dropbtn} onClick={showingDropdown}>
+      {user.userState.username}
+      <FontAwesomeIcon
+          icon={faArrowCircleDown}
+          className={styles.arrowDown}
+        />
+         </button>
+      <div id={styles.myDropdown} className={styles.dropdowncontent}>
+      <button type='button' onClick={signOut}>Sign Out</button>
+      </div>
+      
     </div>
   );
 }
