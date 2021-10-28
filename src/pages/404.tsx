0425b45975bda3c6b3607045f57853f4d815ca68
Liam Fontes <liamfontes1244@gmail.com>
2021-10-28 10:37:59 -0400

    refactored SEO to use one header component for modularity

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import Head from 'next/head';
 import Image from 'next/image';
 import saddog from './saddog.jpg';
 import styles from '../styles/LandingPage.module.scss';
+import HTMLHeader from '../components/Globals/HTMLHeader';
 
 function Four04() {
   return (
     <div className={styles.invalidPage}>
-      <Head>
-        <title> Revis - Redis Performance Visualization Tool</title>
-        <meta
-          name="keywords"
-          content="HTML, CSS, JavaScript, Redis, caching, optimization, tools, visualization, visualizer, visual, performance, improvement"
-        ></meta>
-        <meta
-          name="description"
-          content="Redis performance enhancer visualization tool"
-        ></meta>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-        <meta
-          name="author"
-          content="Liam Fontes, Mercedes Kalaizic, Chao Yu, Jason Zeng"
-        ></meta>
-      </Head>
+      <HTMLHeader />
       <h1>404 error</h1>
       <h2>Oh no! Page not found</h2>
       <Image
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 import { GlobalProvider } from '../context/Provider';
 import '../styles/globals.scss';
 import DarkModeToggle from '../components/Globals/DarkModeToggle';
+import HTMLHeader from '../components/Globals/HTMLHeader';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="general">
-      <Head>
-        <title> Revis - Redis Performance Visualization Tool</title>
-        <meta
-          name="keywords"
-          content="HTML, CSS, JavaScript, Redis, caching, optimization, tools, visualization, visualizer, visual, performance, improvement"
-        ></meta>
-        <meta
-          name="description"
-          content="Redis performance enhancer visualization tool"
-        ></meta>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-        <meta
-          name="author"
-          content="Liam Fontes, Mercedes Kalaizic, Chao Yu, Jason Zeng"
-        ></meta>
-      </Head>
+      <HTMLHeader />
       <GlobalProvider>
         <ThemeProvider enableSystem={false}>
           <DarkModeToggle />
