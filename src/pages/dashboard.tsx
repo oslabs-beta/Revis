dafd388b5a7eb9d8c23d633b8246e61219247cab
Liam Fontes <liamfontes1244@gmail.com>
2021-10-28 10:26:33 -0400

    Added SEO optimzation

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import Head from 'next/head';
 import Dashboard from '../components/Dashboard/Dashboard';
 
 export default function dashboard() {
-  return <Dashboard />;
+  return (
+    <div>
+      <Head>
+        <title> Revis - Redis Performance Visualization Tool</title>
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, Redis, caching, optimization, tools, visualization, visualizer, visual, performance, improvement"
+        ></meta>
+        <meta
+          name="description"
+          content="Redis performance enhancer visualization tool"
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta
+          name="author"
+          content="Liam Fontes, Mercedes Kalaizic, Chao Yu, Jason Zeng"
+        ></meta>
+      </Head>
+      <Dashboard />
+    </div>
+  );
 }
--- src/pages/graphs.tsx
+++ src/pages/graphs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import GraphContainer from '../components/Graphs/Singular/GraphContainer';
 import Welcome from '../components/Globals/Welcome';
 import BackButton from '../components/Globals/BackButton';
@@ -7,11 +8,29 @@ import UpdateInterval from '../components/Globals/UpdateInterval';
 export default function graphs() {
   return (
     <div>
+      <Head>
+        <title> Revis - Redis Performance Visualization Tool</title>
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, Redis, caching, optimization, tools, visualization, visualizer, visual, performance, improvement"
+        ></meta>
+        <meta
+          name="description"
+          content="Redis performance enhancer visualization tool"
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta
+          name="author"
+          content="Liam Fontes, Mercedes Kalaizic, Chao Yu, Jason Zeng"
+        ></meta>
+      </Head>
       <BackButton />
       <Welcome />
       <GraphContainer />
       <UpdateInterval />
-    
     </div>
   );
 }
