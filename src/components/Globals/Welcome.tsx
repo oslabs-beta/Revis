f29634b38ab48604dd2079f4d52456a87bc9da83
Liam Fontes <liamfontes1244@gmail.com>
2021-10-28 10:13:42 -0400

    resolved merge conflicts

--- src/components/Globals/Welcome.tsx
+++ src/components/Globals/Welcome.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
+import router from 'next/router';
 import { useStore } from '../../context/Provider';
 import { Context } from '../../context/interfaces';
 import styles from '../../styles/Welcome.module.scss';
-import router from 'next/router';
 
 function Welcome() {
   const { user }: Context = useStore();
@@ -19,7 +19,7 @@ function Welcome() {
   return (
     <div className={styles.Welcome}>
       <h4>Signed in as </h4>
-      <button type='button' id={styles.dropbtn} onClick={showingDropdown}>
+      <button type="button" id={styles.dropbtn} onClick={showingDropdown}>
         {user.userState.username}
         <FontAwesomeIcon
           icon={faArrowCircleDown}
@@ -27,11 +27,7 @@ function Welcome() {
         />
       </button>
       <div id={styles.myDropdown} className={styles.dropdowncontent}>
-<<<<<<< HEAD
-        <button type='button' onClick={signOut}>
-=======
         <button type="button" onClick={signOut}>
->>>>>>> liam/launch
           Sign Out
         </button>
       </div>
