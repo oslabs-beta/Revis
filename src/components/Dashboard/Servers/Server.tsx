2f1e9cb9caaa724a8de559920eb246671d652b74
Liam Fontes <liamfontes1244@gmail.com>
2021-10-29 12:49:41 -0400

    Fixed guest servers

--- src/components/Dashboard/Servers/Server.tsx
+++ src/components/Dashboard/Servers/Server.tsx
@@ -16,23 +16,24 @@ export default function Server(props) {
   const { userState } = user;
 
   const removeServer = (e) => {
-    // if (
-    //   userState.username === 'GuestUser' &&
-    //   (e.target.name === 'Test' || e.target.id === 'Test')
-    // ) {
-    //   const messageDiv = document.querySelector('#guestMessage');
-    //   messageDiv.style.border = 'solid var(--grey)';
-    //   messageDiv.innerHTML =
-    //     'Test server cannot be deleted under the guest account.';
-    //   setTimeout(() => {
-    //     messageDiv.innerHTML = '';
-    //     messageDiv.style.border = '';
-    //   }, 3000);
-    // } else {
-    serversDispatch({
-      type: 'deleteServer',
-      message: { name: e.target.id },
-    });
+    if (
+      userState.username === 'GuestUser' &&
+      (e.target.name === 'Test' || e.target.id === 'Test')
+    ) {
+      const messageDiv = document.querySelector('#guestMessage');
+      messageDiv.style.border = 'solid var(--grey)';
+      messageDiv.innerHTML =
+        'Test server cannot be deleted under the guest account.';
+      setTimeout(() => {
+        messageDiv.innerHTML = '';
+        messageDiv.style.border = '';
+      }, 3000);
+    } else {
+      serversDispatch({
+        type: 'deleteServer',
+        message: { name: e.target.id },
+      });
+    }
   };
   const removeServerAnimation = (e) => {
     const wrapperName: HTMLDivElement = e.target.attributes[1].value;
