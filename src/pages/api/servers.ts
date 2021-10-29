0bb82270e5de5119dea57ed7b98770e5da5dcaa6
Liam Fontes <liamfontes1244@gmail.com>
2021-10-29 11:51:43 -0400

    updated mobile styling

--- src/pages/api/servers.ts
+++ src/pages/api/servers.ts
@@ -61,7 +61,7 @@ const servers = async (req: NextApiRequest, res: NextApiResponse) => {
         return res.status(200).json({ success: true });
       } catch (err) {
         console.log(`FAILED QUERY ${SQLquery}`);
-        return res.status(400).json({ success: false, error: err });
+        return res.status(400).json({ success: false });
       }
     default:
       return res.status(400).json({ success: false, error: 'Invalid request' });
