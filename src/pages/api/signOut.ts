840b66aebffc56f94754bb3002c22ff52fab7277
LiamFontes <liamfontes1244@gmail.com>
2021-11-28 22:01:44 -0500

    initial progress on API rework

--- src/pages/api/signOut.ts
+++ src/pages/api/signOut.ts
@@ -2,13 +2,20 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import Cookies from 'cookies';
 
 const signOut = async (req: NextApiRequest, res: NextApiResponse) => {
-  const cookies: Cookies = new Cookies(req, res);
-  cookies.set('username', null);
-  cookies.set('ssid', null);
-  cookies.set('session', null);
-  cookies.set('serverID', null);
-  cookies.set('previouslyCalled', null);
-  cookies.set('lastCalled', null);
-  return res.status(200).json({ success: true });
+	const cookies: Cookies = new Cookies(req, res);
+
+	const cookieNameList = [
+		'username',
+		'ssid',
+		'session',
+		'serverID',
+		'previouslyCalled',
+		'lastCalled',
+	];
+	cookieNameList.forEach((cookie: Cookies) => {
+		cookies.set(cookie, null);
+	});
+
+	return res.status(200).json({ success: true });
 };
 export default signOut;
